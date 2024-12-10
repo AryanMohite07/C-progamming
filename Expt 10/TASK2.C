d0b974d2a01f117d2290147026bfbc7d3ec4e4c4
AryanMohite07 <aryan07@eng.rizvi.edu.in>
2024-12-10 18:01:30 +0530

    Create TASK2.C

---
+++ Expt 10/TASK2.C
@@ -0,0 +1,22 @@
+*** Array Reverser ***
+
+Enter the size of the array: 5
+
+Enter the elements of the array:
+
+0 : 2
+
+1 : 4
+
+2 : 3
+
+3 : 5
+
+4 : 6
+
+Array elements in reverse order:
+
+6 5 3 4 2
+
+*/
+/*
