859fd046d6dca513bec9fe53a5caf377a106c957
AryanMohite07 <aryan07@eng.rizvi.edu.in>
2024-12-10 18:03:58 +0530

    Create TASK1.C

---
+++ Expt 11/TASK1.C
@@ -0,0 +1,47 @@
+***  Database ***
+Employee Database Menu:
+1. Add Employee Record
+2. Display Employee Records
+3. Exit
+Enter your choice: 1
+Enter Employee ID: 101
+Enter Employee Name: Sanjay Singh
+Enter Employee Salary: 20000
+Employee record added successfully!
+Employee Database Menu:
+1. Add Employee Record
+2. Display Employee Records
+3. Exit
+Enter your choice: 1
+Enter Employee ID: 102
+Enter Employee Name: Junaid
+Enter Employee Salary: 230000
+Employee record added successfully!
+Employee Database Menu:
+1. Add Employee Record
+2. Display Employee Records
+3. Exit
+Enter your choice: 1
+Enter Employee ID: 103
+Enter Employee Name: Jon Doe
+Enter Employee Salary: 12000
+Employee record added successfully!
+Employee Database Menu:
+1. Add Employee Record
+2. Display Employee Records
+3. Exit
+Enter your choice: 2
+Employee Records:
+ID Name Salary
+---------------------------------
+101 Sanjay Singh 20000.00
+102 Junaid 230000.00
+103 Jon Doe 12000.00
+Employee Database Menu:
+1. Add Employee Record
+2. Display Employee Records
+3. Exit
+Enter your choice: 3
+Exiting the program.
+*/
+/*
